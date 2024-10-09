f1712c32e4267c6a373d19e1abbcd3dcbbd9674d
liiiiiiira <131793430+liiiiiiira@users.noreply.github.com>
2024-10-08 21:37:18 -0300

    Add files via upload

--- dng_geo.R
+++ dng_geo.R
@@ -1,67 +1,66 @@
 # Packages ====
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(tidyverse, read.dbc, janitor, geobr, lme4, scales,
-               ggbreak, readxl, survminer, R0, gghighlight, brpop)
+pacman::p_load(read.dbc, tidyverse, janitor, readxl, geobr, scales, viridis,
+               gtsummary, lme4, expss, gt, modelsummary)
 
-# Referência https://doi.org/10.1590/0102-311XPT261921
-# Fonte dos DBCs: https://datasus.saude.gov.br/transferencia-de-arquivos
-# Fonte do GeoSES: https://journals.plos.org/plosone/article/file?type=supplementary&id=10.1371/journal.pone.0232074.s003
+# Reference https://doi.org/10.1590/0102-311XPT261921
+# DBC source: https://datasus.saude.gov.br/transferencia-de-arquivos
+# GeoSES source: https://journals.plos.org/plosone/article/file?type=supplementary&id=10.1371/journal.pone.0232074.s003
 
 # Databases ====
 ## Raw .dbc files ====
-deng14 <- read.dbc("DENGBR14.dbc")
-deng15 <- read.dbc("DENGBR15.dbc")
-deng16 <- read.dbc("DENGBR16.dbc")
-deng17 <- read.dbc("DENGBR17.dbc")
-deng18 <- read.dbc("DENGBR18.dbc")
-deng19 <- read.dbc("DENGBR19.dbc")
-deng20 <- read.dbc("DENGBR20.dbc")
-deng21 <- read.dbc("DENGBR21.dbc")
-deng22 <- read.dbc("DENGBR22.dbc")
-deng23 <- read.dbc("DENGBR23.dbc")
+dng14 <- read.dbc("DENGBR14.dbc")
+dng15 <- read.dbc("DENGBR15.dbc")
+dng16 <- read.dbc("DENGBR16.dbc")
+dng17 <- read.dbc("DENGBR17.dbc")
+dng18 <- read.dbc("DENGBR18.dbc")
+dng19 <- read.dbc("DENGBR19.dbc")
+dng20 <- read.dbc("DENGBR20.dbc")
+dng21 <- read.dbc("DENGBR21.dbc")
+dng22 <- read.dbc("DENGBR22.dbc")
+dng23 <- read.dbc("DENGBR23.dbc")
 
 ## Merging bases with equivalent lengths and removing originals ====
-deng14a20 <- rbind(deng14,deng15,deng16,deng17,deng18,deng19,deng20)|>
+dng14a20 <- rbind(dng14, dng15, dng16, dng17, dng18, dng19, dng20)|>
   janitor::clean_names()
-rm(deng14,deng15,deng16,deng17,deng18,deng19,deng20)
+rm(dng14, dng15, dng16, dng17, dng18, dng19, dng20)
 
-deng21a23 <- rbind(deng21,deng22,deng23)|>
+dng21a23 <- rbind(dng21, dng22, dng23)|>
   janitor::clean_names()|>
   select(-dt_digita, -migrado_w)
-rm(deng21,deng22,deng23)
+rm(dng21, dng22, dng23)
 
 ## Filters ====
 ### Dengue without warning signs (10), with warning signs (11) and with severity signs (12) ====
-deng14a20 <- deng14a20|>
+dng14a20 <- dng14a20|>
   filter(classi_fin %in% c(10, 11, 12))
 
-deng21a23 <- deng21a23|>
+dng21a23 <- dng21a23|>
   filter(classi_fin %in% c(10, 11, 12))
 
 ### Laboratory confirmed cases ====
-deng14a20 <- deng14a20|>
+dng14a20 <- dng14a20|>
   filter(criterio == 1)
 
-deng21a23 <- deng21a23|>
+dng21a23 <- dng21a23|>
   filter(criterio == 1)
 
-# Now the ACTUALLY confirmed
 labvars <- c("resul_prnt", "resul_soro", "resul_ns1", "resul_vi_n",
              "resul_pcr", "histopa_n", "imunoh_n")
 
-deng14a20 <- deng14a20|>
+dng14a20 <- dng14a20|>
   filter(!if_all(labvars, .fns = ~ is.na(.)|. == 4))
 
-deng21a23 <- deng21a23|>
+dng21a23 <- dng21a23|>
   filter(!if_all(labvars, .fns = ~ is.na(.)|. == 4))
 rm(labvars)
 
 ## Compatibilizing, merging, selecting, reordering and mutating variables ====
-deng14a20 <- deng14a20|>
+dng14a20 <- dng14a20|>
   mutate(dt_nasc = str_sub(dt_nasc, start = 1, end = 4))|>
   rename(ano_nasc = dt_nasc)
 
-dng_tot <- rbind(deng14a20,deng21a23)
+dng_tot <- rbind(dng14a20, dng21a23)
 
 dng_tot <- dng_tot|>
   dplyr::select(tp_not, id_agravo, dt_notific, sg_uf_not, id_municip, dt_sin_pri,
@@ -89,9 +88,9 @@ dng_tot <- dng_tot|>
            tp_idade == 3 ~ val_idade/12,
            tp_idade == 2 ~ val_idade/365,
            tp_idade == 1 ~ val_idade/(365*24),
-           TRUE ~ NA
-         ))|>
-  dplyr::select(-tp_idade, -val_idade)
+           TRUE ~ NA),
+         cs_sexo = factor(cs_sexo, levels = c("F", "M")))|>
+  dplyr::select(-tp_idade, -val_idade, -nu_idade_n)
 
 start_date <- as.Date("2014-01-01")
 end_date <- as.Date("2023-12-31")
@@ -105,9 +104,11 @@ dng_tot <- dng_tot|>
 ## Adding the geo_ses variable to the dataframe ====
 geo_ses <- read_excel("pone.0232074.s003.xls")|>
   janitor::clean_names()|>
-  mutate(faixa_geoses = cut(geo_ses,
-                            breaks = seq(-1, 1, length.out = n_breaks+1),
-                            labels = str_to_upper(letters[n_breaks:1])),
+  mutate(faixa_geoses = ordered(cut(geo_ses,
+                                    breaks = seq(min(geo_ses),
+                                                 max(geo_ses),
+                                                 length.out = n_breaks+1),
+                                    labels = str_to_upper(letters[n_breaks:1]))),
          across(c(munic_code6, munic_code7), .fns = ~ factor(.)))|>
   dplyr::select(munic_code6, munic_code7, geo_ses, faixa_geoses)
 cities <- data.frame(
@@ -117,6 +118,35 @@ cities <- data.frame(
   abbrev_state = geobr::read_municipality()$abbrev_state)
 geo_ses <- left_join(geo_ses, cities, by = join_by(munic_code7 == code_muni))
 dng_tot <- left_join(dng_tot, geo_ses, by = join_by(id_mn_resi == munic_code6))
+
+### Plotting GeoSES ====
+geo_ses <- left_join(geo_ses, geobr::read_municipality()|>
+                       select(code_muni, geom)|>
+                       mutate(code_muni = as.factor(code_muni)), by = join_by(munic_code7 == code_muni))
+map_absolute <- ggplot(geo_ses)+
+  geom_sf(aes(fill = geo_ses,
+              geometry = geom),
+          color = "#00000000")+
+  scale_fill_viridis(option = "inferno")+
+  theme_void()+
+  labs(fill = "GeoSES")+
+  theme(legend.position = "bottom",
+        legend.title.position = "top",
+        legend.direction = "horizontal",
+        legend.title = element_text(hjust = 0.5),
+        legend.text = element_text(hjust = 0.5),
+        legend.frame = element_rect(color = "black", linewidth = 1.2))
+
+windowsFonts(OpenSans = windowsFont("Open Sans"))
+
+png(filename = "idweek2024/map_absolute.png",
+    width = 40, height = 50, units = 'cm', res = 600)
+map_absolute+
+  theme(legend.key.width = unit(2.5, "cm"),
+        legend.key.height = unit(1.5, "cm"),
+        text = element_text(size = 30, family = "OpenSans"))
+dev.off()
+
 rm(cities, geo_ses)
 
 # Mixed-effects modeling ====
@@ -161,22 +191,213 @@ dng_glmer2 <- dng_tot|>
 keys <- dng_glmer2|>
   group_by(id_municip)|>
   summarise(n = n())|>
-  filter(n > 1000)|>
+  filter(n > 800)|>
   dplyr::select(id_municip)|>
-  unlist() # Keys = cities where at least 1000 lab confirmed cases were notified throughout the study period
+  unlist() # Keys = cities where at least 800 confirmed cases were notified throughout the study period
 
 dng_glmer2 <- dng_glmer2|>
   filter(id_municip %in% keys)|>
   group_by(id_municip)|>
-  slice_sample(n = 1000) # Randomly select 1000 cases from these cities to reduce our sample and model with a smaller RAM
+  slice_sample(n = ceiling(250000/length(keys)))|> # Randomly select 460 cases from these cities to reduce our sample and model with a smaller RAM
+  ungroup()
 
-glmer2 <- lme4::glmer(evolucao ~ idade_anos + cs_sexo + cs_gestant + cs_raca +
-                        diabetes + hematolog + hepatopat + renal + hipertensa +
-                        acido_pept + auto_imune + (1|faixa_geoses),
-                      data = dng_glmer2,
-                      family = binomial) # Hopefully this runs more smoothly
+# load("C:/Users/glira/Documents/Análises, apresentações, textos em R/Dados públicos (AIH, SINAN)/Agravos de notificação/Dengue/.RData")
 
-summary(glmer2)
+tbl1_1 <- dng_glmer2|>
+  filter(classi_fin %in% c(10,11,12),
+         cs_sexo %in% c("M", "F"))|>
+  gtsummary::select(idade_anos, cs_sexo, cs_gestant, cs_raca,
+                    diabetes:hipertensa)|>
+  mutate(cs_sexo = factor(cs_sexo,
+                          levels = c("F", "M"),
+                          labels = c("Female", "Male")),
+         cs_raca = factor(cs_raca,
+                          levels = c("B", "NB", "I"),
+                          labels = c("White", "Non-white", "Ignored")))|>
+  expss::apply_labels(idade_anos = "Age", cs_sexo = "Biological sex", 
+                      cs_gestant = "Pregnant", cs_raca = "Race",
+                      diabetes = "Diabetes", hematolog = "Hematologic disease",
+                      hepatopat = "Liver disease", renal = "Kidney disease",
+                      hipertensa = "Hypertension") |> 
+  tbl_summary(missing = "no") |> 
+  modify_header(label ~ "")
 
+gt::gtsave(as_gt(tbl1_1), filename = "idweek2024/table1_1.png")
+
+tbl1_2 <- dng_glmer2|>
+  filter(classi_fin %in% c(10,11,12),
+         cs_sexo %in% c("M", "F"))|>
+  gtsummary::select(acido_pept, auto_imune, faixa_geoses, classi_fin, evolucao)|>
+  mutate(classi_fin = factor(classi_fin,
+                             levels = c(10, 11, 12),
+                             labels = c("Dengue without warning signs", "Dengue with warning signs", "Severe dengue")),
+         evolucao = factor(evolucao,
+                           levels = c(0,1),
+                           labels = c("Death attributed to dengue", "Cured")),
+         faixa_geoses = fct_rev(faixa_geoses))|>
+  expss::apply_labels(acido_pept = "Peptic ulcer disease", auto_imune = "Autoimmune disease",
+                      faixa_geoses = "GeoSES range", classi_fin = "Classification",
+                      evolucao = "Outcome") |> 
+  tbl_summary(missing = "no") |> 
+  modify_header(label ~ "")
+
+gt::gtsave(as_gt(tbl1_2), filename = "idweek2024/table1_2.png")
+
+dng_glmer2|>
+  gtsummary::select(idade_anos, cs_sexo, cs_gestant, cs_raca,
+         diabetes:auto_imune, faixa_geoses, classi_fin)|>
+  filter(classi_fin %in% c(10,11,12),
+         cs_sexo %in% c("M", "F"))|>
+  mutate(cs_sexo = factor(cs_sexo,
+                          levels = c("F", "M"),
+                          labels = c("Female", "Male")),
+         cs_raca = factor(cs_raca,
+                          levels = c("B", "NB", "I"),
+                          labels = c("White", "Non-white", "Ignored")),
+         classi_fin = factor(classi_fin,
+                             levels = c(10, 11, 12),
+                             labels = c("Dengue without warning signs", "Dengue with warning signs", "Severe dengue")),
+         faixa_geoses = fct_rev(faixa_geoses))|>
+  expss::apply_labels(idade_anos = "Age", cs_sexo = "Biological sex", 
+                      cs_gestant = "Pregnant", cs_raca = "Race",
+                      diabetes = "Diabetes", hematolog = "Hematologic disease",
+                      hepatopat = "Liver disease", renal = "Kidney disease",
+                      hipertensa = "Hypertension", acido_pept = "Peptic ulcer disease",
+                      auto_imune = "Autoimmune disease", classi_fin = "Classification") |> 
+  tbl_summary(by = faixa_geoses, missing = "no") |>
+  modify_header(label ~ "") |>
+  add_p()
+
+dng_glmer2|>
+  gtsummary::select(idade_anos, cs_sexo, cs_gestant, cs_raca,
+                    diabetes:auto_imune, geo_ses, classi_fin)|>
+  filter(classi_fin %in% c(10,11,12),
+         cs_sexo %in% c("M", "F"))|>
+  mutate(cs_sexo = factor(cs_sexo,
+                          levels = c("F", "M"),
+                          labels = c("Female", "Male")),
+         cs_raca = factor(cs_raca,
+                          levels = c("B", "NB", "I"),
+                          labels = c("White", "Non-white", "Ignored")),
+         classi_fin = factor(classi_fin,
+                             levels = c(10, 11, 12),
+                             labels = c("Dengue without warning signs", "Dengue with warning signs", "Severe dengue")))|>
+  expss::apply_labels(idade_anos = "Age", cs_sexo = "Biological sex", 
+                      cs_gestant = "Pregnancy status", cs_raca = "Race",
+                      diabetes = "Diabetes", hematolog = "Hematologic disease",
+                      hepatopat = "Liver disease", renal = "Kidney disease",
+                      hipertensa = "Hypertension", acido_pept = "Peptic ulcer disease",
+                      auto_imune = "Autoimmune disease", classi_fin = "Classification") |> 
+  tbl_summary(by = classi_fin, missing = "no") |>
+  modify_header(label ~ "") |>
+  add_p()
+
+glmer2 <- glmer(evolucao ~ idade_anos + cs_sexo + cs_gestant + cs_raca +
+                  diabetes + hematolog + hepatopat + renal + hipertensa +
+                  acido_pept + auto_imune + (1|faixa_geoses),
+                data = dng_glmer2,
+                family = binomial)
+
+options(scipen = 0)
+
+as.double(summary(glmer2)$varcor$faixa_geoses)|> # Shows low baseline variance
+  format(scientific = F)
+as.data.frame(cbind(odds = coef(summary(glmer2))[, "Estimate"],
+                    p_vals = coef(summary(glmer2))[, "Pr(>|z|)"]))|>
+  arrange(odds)|>
+  format(scientific = F, width = 5)
+
+# Proportional odds logistic regression ====
 ## Risk factors versus WHO disease classification ====
-# To be continued...
+dng_polr <- dng_tot|>
+  filter(if_all(diabetes:auto_imune, #Known risk factors should not be empty
+                ~ !is.na(.)),
+         classi_fin %in% c(10,11,12),
+         cs_sexo %in% c("M", "F"))|>
+  mutate(across(diabetes:auto_imune,
+                ~ if_else(.==1, 1, 0)),
+         classi_fin = ordered(classi_fin, levels = c("10", "11", "12")),
+         cs_gestant = if_else(cs_gestant %in% c(1, 2, 3, 4), 1, 0), # 1st, 2nd, 3rd trimester and unknown gestational age versus else
+         cs_raca = case_when(cs_raca == 1 ~ "B", #Whites
+                             cs_raca %in% c(2, 3, 4, 5) ~ "NB", #Non-whites
+                             cs_raca == 9 ~ "I")) |>  #Ignored race
+  mutate(cs_sexo, cs_raca, classi_fin)
+
+dng_polr <- dng_polr|>
+  filter(id_municip %in% keys)|>
+  group_by(id_municip)|>
+  slice_sample(n = ceiling(250000/length(keys)))|> # Randomly select 443 cases from these cities to reduce our sample and model with a smaller RAM
+  ungroup()
+
+dng_polr <- dng_polr|>
+  mutate(across(.cols = c(cs_sexo, cs_gestant, cs_raca, diabetes, hematolog,
+                          hepatopat, renal, hipertensa, acido_pept, auto_imune),
+                .fns = ~ factor(.))) |> 
+  expss::apply_labels(cs_sexo = "Male", cs_gestant = "Pregnancy",
+                      cs_raca = "Race (ignored)", cs_raca = "Race (non-white)",
+                      diabetes = "Diabetes", hematolog = "Hematologic disease",
+                      hepatopat = "Liver disease", renal = "Kidney disease",
+                      hipertensa = "Hypertension", acido_pept = "Peptic ulcer disease",
+                      auto_imune = "Autoimmune disease", geo_ses = "GeoSES",
+                      faixa_geoses = "GeoSES (range)")
+
+polr1 <- MASS::polr(classi_fin ~ idade_anos + cs_sexo + cs_gestant + cs_raca +
+                      diabetes + hematolog + hepatopat + renal + hipertensa +
+                      acido_pept + auto_imune,
+                    data = dng_polr, Hess = TRUE)
+
+cbind(odds = (exp(coef(summary(polr1))[, "Value"])),
+      p_value = (1-pnorm(abs(coef(summary(polr1))[, "t value"]), 0, 1))*2)|>
+  as.data.frame()|>
+  arrange(odds)|>
+  format(scientific = F)
+
+polr2 <- MASS::polr(classi_fin ~ idade_anos + cs_sexo + cs_gestant + cs_raca +
+                      diabetes + hematolog + hepatopat + renal + hipertensa +
+                      acido_pept + auto_imune + geo_ses,
+                    data = dng_polr, Hess = TRUE)
+
+cbind(odds = (exp(coef(summary(polr2))[, "Value"])),
+      p_value = (1-pnorm(abs(coef(summary(polr2))[, "t value"]), 0, 1))*2)|>
+  as.data.frame()|>
+  arrange(odds)|>
+  format(scientific = F)
+
+polr3 <- MASS::polr(classi_fin ~ idade_anos + cs_sexo + cs_gestant + cs_raca +
+                      diabetes + hematolog + hepatopat + renal + hipertensa +
+                      acido_pept + auto_imune + faixa_geoses,
+                    data = dng_polr, Hess = TRUE)
+
+cbind(odds = (exp(coef(summary(polr3))[, "Value"])),
+      p_value = (1-pnorm(abs(coef(summary(polr3))[, "t value"]), 0, 1))*2)|>
+  as.data.frame()|>
+  arrange(odds)|>
+  format(scientific = F)
+
+tbl2 <- modelsummary(
+  models = list(
+    "GLM" = glmer2,
+    "POLR w/o GeoSES" = polr1,
+    "POLR w/ linear GeoSES" = polr2,
+    "POLR w/ stratified GeoSES" = polr3),
+  fmt = 2,
+  estimate  = "{estimate} ({p.value})",
+  statistic = NULL,
+  coef_omit = c(1,14,15),
+  coef_rename = c("idade_anos" = "Age", "cs_sexoM" = "Male", "cs_gestant" = "Pregnancy",
+                  "cs_gestant1" = "Pregnancy", "cs_racaI" = "Race (ignored)",
+                  "cs_racaNB" = "Race (non-white)", "diabetes" = "Diabetes",
+                  "diabetes1" = "Diabetes", "hematolog" = "Hematologic disease",
+                  "hematolog1" = "Hematologic disease", "hepatopat" = "Liver disease",
+                  "hepatopat1" = "Liver disease", "renal" = "Kidney disease",
+                  "renal1" = "Kidney disease", "hipertensa" = "Hypertension",
+                  "hipertensa1" = "Hypertension", "acido_pept" = "Peptic ulcer disease",
+                  "acido_pept1" = "Peptic ulcer disease", "auto_imune" = "Autoimmune disease",
+                  "auto_imune1" = "Autoimmune disease", "geo_ses" = "GeoSES",
+                  "faixa_geoses.L" = "GeoSES (range, linear regression)",
+                  "faixa_geoses.Q" = "GeoSES (range, quadratic regression)",
+                  "faixa_geoses.C" = "GeoSES (range, cubic regression)",
+                  "SD (Intercept faixa_geoses)" = "SD (Intercept GeoSES)"),
+  title = "Summary of model estimates",
+  notes = "p-values for each estimate are indicated in parentheses",
+  output = "idweek2024/table2.png")
